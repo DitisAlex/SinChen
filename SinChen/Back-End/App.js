77ec88a19642ff1b9821243f9456e7d2149e9729
DitisAlex <alexcheng0314@gmail.com>
2021-06-30 02:58:54 +0200

    Setting up server for droplet

--- SinChen/Back-End/App.js
+++ SinChen/Back-End/App.js
@@ -9,6 +9,7 @@ const cors = require("cors")
 const bodyParser = require("body-parser")
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
+const path = require('path')
 
 // Initialize CORS
 app.use(cors({ origin: true, credentials: true }));
@@ -21,13 +22,15 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 // Initialize routes
+app.use(express.static(`${__dirname}/../Front-End/build`));
 app.use("/api/users", require("./routes/users"))
 app.use("/api/orders", require("./routes/orders"))
 app.use("/api/items", require("./routes/items"))
+app.get('*', (req, res)=>{  res.sendFile(path.join(__dirname, '../Front-End/build/index.html'));})
 
 // Initialize port
 app.listen(port, () => mongoose.connect(
-  dbUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}, () => {
+  dbUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
     console.log(`Database connection established`)
   }
 ));
